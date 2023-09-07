b13a342021d1e755c4edaee9b876ed508be61d11
The-Lonesome-Coder <the.lonesome.coder@gmail.com>
2023-09-07 15:59:28 +0800

    Exercise 2-2.cpp

---
+++ 2-2.cpp
@@ -0,0 +1,27 @@
+#include <cmath>
+#include <format>
+#include <iostream>
+#include <numbers>
+
+
+// Function for calculating the area of circle
+double circle_area(double radius)
+{
+    return pow(radius, 2.0) * std::numbers::pi;
+}
+
+
+int main(void)
+{
+    double radius = 0.0, area = 0.0;
+
+    std::cout << "Enter radius: " << std::endl;
+    std::cin >> radius;
+
+    area = circle_area(radius);
+
+    std::cout << std::format("The area of a circle with radius {:.2f} is {:.5f}", radius, area)
+              << std::endl;
+
+    return 0;
+}
