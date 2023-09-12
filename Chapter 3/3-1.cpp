32aafe716c2c6beaa332aa6dc162a9991cdb2b47
The-Lonesome-Coder <the.lonesome.coder@gmail.com>
2023-09-12 11:50:53 +0800

    Exercise 3-1

---
+++ Chapter 3/3-1.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <format>
+
+
+int main(void)
+{
+    int value = 50;
+
+    std::cout << "Please enter an integer: ";
+    std::cin >> value;
+
+    const auto inverted{ static_cast<unsigned>(~value) };
+
+    std::cout << std::format(" {:^34} {:^34} {:^34}\n", "value", "~value", "~value + 1");
+    std::cout << std::format(" {:#034b} {:#034b} {:#034b}\n", value, inverted, inverted + 1);
+    std::cout << std::format(" {:^34} {:^34} {:^34}\n", value, ~value, ~value + 1);
+}
