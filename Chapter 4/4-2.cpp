ae3e109ae8970062520a68458beb93729516a584
The-Lonesome-Coder <the.lonesome.coder@gmail.com>
2023-09-13 10:21:15 +0800

    Exercise 4-2

---
+++ Chapter 4/4-2.cpp
@@ -0,0 +1,38 @@
+#include <algorithm>   // std::swap()
+#include <exception>   // std::out_of_range()
+#include <format>
+#include <iostream>
+
+
+
+int main(void)
+{
+    int num1 = 0, num2 = 0;
+
+    std::cout << "Please enter two integers: \n";
+    std::cin >> num1 >> num2;
+
+    // Check validity
+    if (num1 <= 0 || num2 <= 0)
+    {
+        throw std::out_of_range("The input number should be a non-zero positive integer.");
+    }
+
+    // Set num1 to be smaller than num2
+    if (num1 > num2)
+    {
+        std::swap(num1, num2);
+    }
+
+    // Exact multiple
+    if (num2 % num1 == 0)
+    {
+        std::cout << std::format("{} is the exact multiple of {}.", num2, num1)
+                  << std::endl;
+    }
+    else
+    {
+        std::cout << std::format("{} is not the exact multiple of {}.", num2, num1)
+                  << std::endl;
+    }
+}
