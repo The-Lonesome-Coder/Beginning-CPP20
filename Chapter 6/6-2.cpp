6a1006e901a8bcdf9ea7bbe99b39bcccb4f0a475
The-Lonesome-Coder <the.lonesome.coder@gmail.com>
2023-09-17 15:16:27 +0800

    Exercise 6-2

---
+++ Chapter 6/6-2.cpp
@@ -0,0 +1,46 @@
+#include <array>
+#include <format>
+#include <iostream>
+#include <memory>
+
+
+int main(void)
+{
+    const int size = 50;
+
+    std::array<int, size> odd {};
+
+    for (int i = 0; i < size; ++i)
+    {
+        odd[i] = i * 2 + 1;
+    }
+
+    int* pointer = &odd.front();
+
+    for (int i = 0; i < size; ++i)
+    {
+        std::cout << std::format("{:^2} | ", *(pointer++));
+
+        if ((i + 1) % 10 == 0)
+        {
+            std::cout << std::endl;
+        }
+    }
+
+    std::cout << "-------------------------------------------------"
+              << std::endl;
+
+    pointer = &odd.back();
+
+    for (int i = 0; i < size; ++i)
+    {
+        std::cout << std::format("{:^2} | ", *(pointer--));
+
+        if ((i + 1) % 10 == 0)
+        {
+            std::cout << std::endl;
+        }
+    }
+
+    return 0;
+}
