74bb33bbd9ab2bde13f0ed06f39ecf5c9996ea8c
The-Lonesome-Coder <the.lonesome.coder@gmail.com>
2023-09-17 16:10:46 +0800

    Exercise 6-4

---
+++ Chapter 6/6-4.cpp
@@ -0,0 +1,32 @@
+#include <cmath>
+#include <format>
+#include <iostream>
+#include <vector>
+
+
+int main(void)
+{
+    int size = 0;
+
+    std::cout << "Please enter the size of the vector: ";
+    std::cin >> size;
+
+    auto vec = new std::vector<double>(size);
+
+    for(int i = 0; i < size; ++i)
+    {
+        (*vec)[i] = 1.0 / ((i + 1) * (i + 1));
+    }
+
+    double sum = 0;
+
+    for (auto i : * vec)
+    {
+        sum += i;
+    }
+
+    std::cout << std::format("The result is {}", std::sqrt(sum * 6.0))
+              << std::endl;
+
+    delete[] vec;
+}
