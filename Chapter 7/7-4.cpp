a9dc633b971753db6225a77dfb808e01fe709402
The-Lonesome-Coder <the.lonesome.coder@gmail.com>
2023-09-19 20:30:00 +0800

    Exercise 7-4

---
+++ Chapter 7/7-4.cpp
@@ -0,0 +1,57 @@
+#include <algorithm>
+#include <cctype>
+#include <iostream>
+#include <string>
+#include <vector>
+
+
+std::vector<char> char_split(std::string &str)
+{
+    std::vector<char> result {};
+
+    for (auto i : str)
+    {
+        if (i == ' ')
+        {
+            continue;
+        }
+
+        result.push_back(i);
+    }
+
+    return result;
+}
+
+
+int main(void)
+{
+    std::string str1 {};
+    std::string str2 {};
+
+    std::cout << "Please enter the first string: ";
+    std::getline(std::cin, str1);
+
+    std::cout << "Please enter the second string: ";
+    std::getline(std::cin, str2);
+
+    std::vector<char> vec1 = char_split(str1);
+    std::vector<char> vec2 = char_split(str2);
+
+    std::sort(vec1.begin(), vec1.end());
+    std::sort(vec2.begin(), vec2.end());
+
+    bool result = std::equal(vec1.cbegin(), vec1.cend(), vec2.cbegin());
+
+    if (result)
+    {
+        std::cout << "The two strings are anagram."
+                  << std::endl;
+    }
+    else if (!result)
+    {
+        std::cout << "The two strings are not anagram."
+                  << std::endl;
+    }
+
+    return 0;
+}
