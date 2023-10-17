e5056b9d8b1f9d64a1f74e46d2d084475a752e50
The-Lonesome-Coder <the.lonesome.coder@gmail.com>
2023-10-17 19:38:31 +0800

    Exercise 16-1

---
+++ Chapter 16/16-1/main.cpp
@@ -0,0 +1,48 @@
+import <functional>;
+import <iostream>;
+import <random>;
+
+import curveball;
+
+
+// Throw exception randomly - about 25% of the time
+auto createUniformPseudoRandomNumberGenerator(double min, double max)
+{
+    std::random_device seeder;          // True random number generator to obtain a seed (slow)
+    std::default_random_engine generator { seeder() };      // Efficient pseudo-random generator
+    std::uniform_real_distribution distribution { min, max }; // Generate in [min, max) interval
+
+    return std::bind(distribution, generator);             //... and in the darkness bind them!
+}
+
+
+// Throw a Curveball exception 25% of the time
+void throwCurveballSometimes()
+{
+    static auto random = createUniformPseudoRandomNumberGenerator(0, 100);
+
+    if (random() < 25)
+        throw Curveball {};
+}
+
+
+int main(void)
+{
+    size_t number = 1000;                             // Number of loop iterations
+    size_t exceptionCount = 0;                         // Count of exceptions thrown
+
+    for (size_t i {}; i < number; ++i)
+    {
+        try
+        {
+            throwCurveballSometimes();
+        }
+        catch (const Curveball& ex)
+        {
+            std::cout << ex.what() << std::endl;
+            ++exceptionCount;
+        }
+    }
+
+    std::cout << "Curveball exception thrown " << exceptionCount << " times out of " << number << ".\n";
+}
